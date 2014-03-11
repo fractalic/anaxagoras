94643a663e46d496f2c8a052b274e01a06ee82fe
Ben Hughes <bhs.fractal@gmail.com>
2014-03-10 18:10:44 -0700

    renamed some timer functions

--- pwm.c
+++ pwm.c
@@ -6,13 +6,20 @@
 #define BAUD 115600L
 
 //We want timer 0 to interrupt every 100 microseconds ((1/10000Hz)=100 us)
-#define TIMER0_FREQ 10000L
+#define TIMER0_FREQ 10000L // don't change this from 10000L
 //16 bit timer, counting upwards
 #define TIMER0_RELOAD_VALUE (65536L-((XTAL)/(2*TIMER0_FREQ)))
 
 //These variables are used in the ISR
 volatile unsigned char pwmcount;
 volatile unsigned char pwm1;
+
+// a tenths-of-a-second counter
+volatile long int tenths;
+volatile long int tenths_count;
+// a hundredths-of-a-second counter
+volatile long int hundredths;
+volatile long int hundredths_count;
 
 void timer0_init (void)
 {
@@ -52,11 +59,35 @@ void timer0_event (void) interrupt 1 using 1
 	// count timer interrupts
 	if(++pwmcount>99) pwmcount=0;
 
-	P1_6=(pwm1>pwmcount)?1:0;
+	if (drive_left) {
+		motor_left=(motor_left_pwm>pwmcount)?1:0;
+	}
+	if (drive_right) {
+		motor_right = (motor_right_pwm>pwmcount)?1:0;
+	}
+	
+
+	// count tenths of a second since latest reset
+	if(++tenths_count>1000){
+		tenths_count = 0;
+		tenths++;
+	}
+
+	if(++hundredths_count)
 }
 
+// reset the tenths counter
+void reset_time()
+{
+	tenths = 0;
+	hundredths = 0;
+}
+
+/*
 void main (void)
 {
 	timer0_init();
-	pwm1=20; //50% duty cycle wave at 100Hz
+	// not sure I believe the duty cycle calculation, but maybe I just don't know
+	motor_left_pwm=20; //50% duty cycle wave at 100Hz
 }
+*/
