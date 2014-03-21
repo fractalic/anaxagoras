1dd2a0addcd8a937acccbca5eda993cc7aa1365c
Ben Hughes <benhughes@shaw.ca>
2014-03-21 13:49:19 -0700

    trying to create a slow running timer1

--- timer.c
+++ timer.c
@@ -15,6 +15,11 @@
 //16 bit timer, counting upwards
 #define TIMER0_RELOAD_VALUE (65536L-((XTAL)/(2*TIMER0_FREQ)))
 
+//We want timer 1 to interrupt every 10000 microseconds ((1/100Hz)=10000 us)
+#define TIMER1_FREQ 100L // don't change this from 100L
+//16 bit timer, counting upwards
+#define TIMER1_RELOAD_VALUE (65536L-((XTAL)/(2*TIMER1_FREQ)))
+
 //These variables are used in the ISR
 volatile unsigned char pwmcount;
 volatile unsigned char left_wheel_pwm;
@@ -53,16 +58,7 @@ void reset_millis();
 void timer0_event (void) interrupt 1 using 1
 {
 	// load the timer and start it
-	//timer0_restart();
-	TF0=0; // Clear the overflow flag
-	
-	TR0=0; // Stop timer 0
-
-	// load the timer
-	TH0=TIMER0_RELOAD_VALUE/0x100; // upper8 bits
-	TL0=TIMER0_RELOAD_VALUE%0x100;
-
-	TR0=1; // Start timer 0
+	timer0_restart();
 
 	// only count 100 ticks before setting to zero
 	// (arbitrary, but should be multiple of 10 and 100)
@@ -77,11 +73,6 @@ void timer0_event (void) interrupt 1 using 1
 		right_wheel = (right_wheel_pwm> (t0_ticks%100))?1:0;
 	}*/
 	
-	// count milliseconds (every ten 100us timer ticks) 
-	if( t0_ticks == 0 ) {
-		millis_v++;
-	}
-	
 }
 
 // timer0_init()
@@ -95,10 +86,55 @@ void timer0_init (void)
 	TMOD=(TMOD&0xf0)|0x01; // 16-bit timer
 	
 	// load a value into timer0 and restart
-	timer0_restart();
+	TF0=0; // Clear the overflow flag
+	TR0=0; // Stop timer 0
+	// load the timer
+	TH0=TIMER0_RELOAD_VALUE/0x100; // upper8 bits
+	TL0=TIMER0_RELOAD_VALUE%0x100;
+	TR0=1; // Start timer 0
 
 	ET0=1; // Enable timer 0 interrupt
 	EA=1;  // Enable global interrupts
+}
+
+// timer1_init()
+// setup the timer0 and begin running it
+void timer1_init (void)
+{
+	// Initialize timer 0 for ISR 'pwmcounter' below
+	TR1=0; // Stop timer 0
+	TF1=0; // Clear the overflow flag
+
+	TMOD=(TMOD&0x0f)|0x10; // 16-bit timer
+	
+	// load a value into timer0 and restart
+	TF1=0; // Clear the overflow flag
+	TR1=0; // Stop timer 0
+	// load the timer
+	TH1=TIMER1_RELOAD_VALUE/0x100; // upper8 bits
+	TL1=TIMER1_RELOAD_VALUE%0x100;
+	TR1=1; // Start timer 0
+
+
+	ET1=1; // Enable timer 1 interrupt
+	EA=1;  // Enable global interrupts
+}
+
+// timer0_event()
+// called whenever the timer overflows
+// the time is determind by the frequency defined at the top of this file
+void timer1_event (void) interrupt 3 using 3
+{
+	// load a value into timer0 and restart
+	TF1=0; // Clear the overflow flag
+	TR1=0; // Stop timer 0
+	// load the timer
+	TH1=TIMER1_RELOAD_VALUE/0x100; // upper8 bits
+	TL1=TIMER1_RELOAD_VALUE%0x100;
+	TR1=1; // Start timer 0
+
+	millis_v++;
+	
 }
 
 // timer0_restart()
